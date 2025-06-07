541903d10374e230ed33f7ec9da3c96f15a16054
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-07 13:40:50 +0200

    feat: update Router configuration to use dynamic basename from environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
+  console.log("command: ", command);
+  const base = command === "build" ? "/mlp-visualizer/" : "/";
+  console.log("base: ", base);
   return {
-    base: command === "build" ? "/mlp-visualizer/" : "/",
+    base,
     plugins: [react(), tailwindcss()],
   };
 });
