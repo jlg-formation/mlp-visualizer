fe4a1d51e2985fcaebf328a6dc8555d3bf9bc6e9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-08 10:53:09 +0200

    Add Board page

---
+++ src/pages/Board.tsx
@@ -0,0 +1,8 @@
+export default function Board() {
+  return (
+    <div className="p-6">
+      <h1 className="text-xl font-bold">Board</h1>
+      <p className="mt-2">This is the board page.</p>
+    </div>
+  );
+}
