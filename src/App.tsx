94211ec3f76feac82bb7cd015b92a501b5abcc7d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-08 16:22:20 +0200

    Remove Playground page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import Playground from "./pages/Playground";
 import Doc from "./pages/Doc";
 import Board from "./pages/Board";
 import Images from "./pages/Images";
@@ -11,9 +10,6 @@ export default function App() {
       <nav className="bg-white shadow p-4 flex gap-4 text-blue-700 font-medium">
         <Link to="/" className="hover:underline">
           Accueil
-        </Link>
-        <Link to="/playground" className="hover:underline">
-          Playground
         </Link>
         <Link to="/board" className="hover:underline">
           Board
@@ -28,7 +24,6 @@ export default function App() {
       <main className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/playground" element={<Playground />} />
           <Route path="/board" element={<Board />} />
           <Route path="/images" element={<Images />} />
           <Route path="/doc" element={<Doc />} />
