fc09122f5184ee3abb7cc8c5e5f530c633dd327c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-06-08 16:57:40 +0200

    Implement code changes to enhance functionality and improve performance

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,18 @@
-import { useState } from "react";
-import { StepMetroBreadcrumb } from "../components/StepMetroBreadcrumb";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const [currentStep, setCurrentStep] = useState(0); // 0 : aucune étape franchie
-  const steps = [
-    "Charger les données",
-    "Séparer les données",
-    "Définir le modèle",
-    "Entrainer le modèle",
-    "Tester le modèle",
-  ];
-
   return (
-    <>
-      <div className="p-6 max-w-3xl mx-auto">
-        <StepMetroBreadcrumb steps={steps} currentStep={currentStep} />
-        <div className="mt-6 flex gap-2 justify-center">
-          {currentStep === 0 ? (
-            <button
-              className="px-4 py-2 bg-blue-500 text-white rounded"
-              onClick={() => setCurrentStep(1)}
-            >
-              Getting Started
-            </button>
-          ) : (
-            <>
-              <button
-                className="px-4 py-2 bg-gray-200 rounded"
-                onClick={() => setCurrentStep((s) => Math.max(0, s - 1))}
-              >
-                Précédent
-              </button>
-              <button
-                className="px-4 py-2 bg-blue-500 text-white rounded"
-                onClick={() =>
-                  setCurrentStep((s) => Math.min(steps.length, s + 1))
-                }
-              >
-                Suivant
-              </button>
-            </>
-          )}
-        </div>
+    <div className="relative flex w-full items-center justify-center bg-white">
+      <img src="/accueil.png" alt="MLP Visualizer" className="h-[40em]" />
+      <div className="absolute top-30 left-30">
+        <Link
+          to="board"
+          className="rounded border px-4 py-2 text-xl hover:bg-gray-100"
+        >
+          {" "}
+          See the Board &gt;
+        </Link>
       </div>
-    </>
+    </div>
   );
 }
